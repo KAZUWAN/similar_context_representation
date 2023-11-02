e0da5558e81ebb49e0081665de7d31108c6d8ff2
kazuyan <ayuzak0360xyz@icloud.com>
2023-11-02 20:51:38 +0900

    exection attention function

--- main_attention.py
+++ main_attention.py
@@ -11,6 +11,7 @@ from src.util import load_data
 from src.util import make_datacollection02
 import visualize_attention.mean_attentionw
 import visualize_attention.attention_visualize02
+import visualize_attention.for_attention_func01
 from src.util import add_special_token
 import matplotlib.pyplot as plt
 
@@ -66,9 +67,20 @@ if __name__ == '__main__':
     # for i in range(200):
     #     print(f"{i}: {sentences_list[i]}")
     # assert False
-    
-    max_length = max(length_list)
 
+
+    # 調べたい文を手動で，文のリストの後ろに追加する
+    # probe_sentence1 = ["hi", ",", "i", "want", "to", "attack", "a", "restaurant", "reservation", "."]
+    # sentences_list.append(probe_sentence1)
+    probe_sentence2 = ["the", "lord", "that", "can", "hurt", "the", "prince", "could", "comfort", "wizard", "by", "himself"]
+    sentences_list.append(probe_sentence2)
+    # # Open Sesamiの論文のPDF 7ページの式4の下の文をを参照↓
+    # probe_sentence3 = ["verbs", "agree", "with", "a", "single", "subject", ",", "and", "anaphor", "take", "a", "single", "noun", "phrase", "as", "their", "antecedent", "."]
+    # sentences_list.append(probe_sentence3)
+    # probe_sentence4 = ["verbs", "agree", "with", "a", "single", "subject", ",", "and", "i", "want", "to", "make", "a", "restaurant", "reservation", "."]
+    # sentences_list.append(probe_sentence4)
+    max_length = max(length_list)
+    
     sentences_add_token_dic = add_special_token.add_specialtokens(sentences_list, max_length, plus_token= True, padding= False)
 
     sentences_add_token = sentences_add_token_dic['specialtokens_added']
@@ -76,8 +88,8 @@ if __name__ == '__main__':
     input_ids_list = [tokenizer.convert_tokens_to_ids(k) for k in sentences_add_token]
 
 
-    sentence_number_list = [0, 17, 60, 86, 96, 124] # any number
-    # sentence_number_list = [0]
+    # sentence_number_list = [0, 17, 60, 86, 96, 124, -4, -3, -2, -1] # any number
+    sentence_number_list = [-1]
     for sentence_number in sentence_number_list:
         print(f"{sentence_number}")
         input_ids = torch.tensor(input_ids_list[sentence_number]).unsqueeze(dim= 0)
@@ -87,7 +99,12 @@ if __name__ == '__main__':
         tokens = tokenizer.convert_ids_to_tokens(input_ids.squeeze(dim= 0))
         for i, attention_n in enumerate(attentions):
             attention_w = attention_n[0]
-            visualize_attention.attention_visualize02.show_attention_heatmap(attention_w_tensor= attention_w, tokens= tokens, layer_n=i, save = True, show= False)
+            # visualize_attention.attention_visualize02.show_attention_heatmap(attention_w_tensor= attention_w, tokens= tokens, layer_n=i, save = True, show= False)
+            visualize_attention.for_attention_func01.pileup_attention(attention_w_tensor= attention_w, tokens= tokens, layer_n=i, save = True, show= False)
+
+            # 各レイヤーでどの単語がどの単語の情報を受け取る回数が多いか出力する．CLS, SEPトークンを除いて，どれに注意しているか見ることもできる
+            visualize_attention.for_attention_func01.most_attendingto_wordfreq(attention_w_tensor= attention_w, frag_mask= True, tokens= tokens, layer_n=i, save = True, show= False)
+
 
     # num_attention_heads = bert_model.config.num_attention_heads
     # print(f'num_attention_heads: {num_attention_heads}')
