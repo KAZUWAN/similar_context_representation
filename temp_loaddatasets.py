d123e100a7c0739145f893bfe08f13efb3ac6a0f
kazuyan <ayuzak0360xyz@icloud.com>
2023-12-14 22:29:29 +0900

    load data and organize

--- temp_loaddatasets.py
+++ temp_loaddatasets.py
@@ -1,8 +1,13 @@
 from datasets import load_dataset_builder
 from datasets import load_dataset
 from transformers import AutoTokenizer
+import torch
 
 
+# cudaが使用可能なら使用する
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# rotten tomatoesという映画を見たレビューの文のロード
 ds_builder = load_dataset_builder("rotten_tomatoes")
 
 print(ds_builder.info.description)
@@ -10,10 +15,23 @@ print(ds_builder.info.features)
 
 dataset = load_dataset("rotten_tomatoes", split="train")
 
+print(f"データセット概要")
 print(dataset)
-print(dataset[0])
+print()
+# print(dataset[0])
 print(dataset[0]["text"])
+print(dataset[1]["text"])
+print(dataset[2]["text"])
+print()
 
+# ここで，トークナイズする前に，文単位でデータを区切りたい
+# たまにj.r.r.のような人の名前？のために使用するピリオドを考慮しなければならない
+# ピリオドが二つ以上ある文がいくつあるか調べる
+# ↑すなわち，データの素性調査
+# とりあえずピリオドが一つ以下の文だけを対象にする
+
+
+# トークン化
 tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 
 def tokenization(example):
@@ -21,5 +39,15 @@ def tokenization(example):
 
 dataset = dataset.map(tokenization, batched=True)
 
-print(dataset[0]["input_ids"])
+# print(dataset[0]["input_ids"])
+
+# PyTorchのために型変換
+dataset.set_format(type="torch", columns=["input_ids", "token_type_ids", "attention_mask", "label"], device=device)
+print(f"トークナイズ後(torch, device選択)")
+print(dataset[0])
 print(tokenizer.convert_ids_to_tokens(dataset[0]["input_ids"]))
+print()
+
+
+
+
